2f10fbf68fe1c44feb9b33b4e51fa6b5759cf58d
박상훈 <bp4sp4@naver.com>
2026-01-29 10:31:08 +0900

    update footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import styles from "./page.module.css";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   const [showFloatingBanner, setShowFloatingBanner] = useState(true);
@@ -841,6 +842,7 @@ export default function Home() {
           </div>
         </div>
       )}
+      <Footer />
     </div>
   );
 }
